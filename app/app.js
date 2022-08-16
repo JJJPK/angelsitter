a23cf20ddb89cb5503682dcc4f5296555a8dbc94
Gomayang <mogan0609@naver.com>
2022-08-16 16:05:13 +0900

    프런트 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //module
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //routing
@@ -10,8 +11,10 @@ const home = require("./src/routes/home");
 //setting app
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.use("/", home); // use -> 미들 웨어 등록 메서드
 
